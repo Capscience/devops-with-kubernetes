2d7f0c5155c2c49b078355472badc37a3eaa010d
Simo Soini <simo@capscience.fi>
2026-01-15 20:22:48 +0200

    Exercise 1.7 submission

--- log-output/README.md
+++ log-output/README.md
@@ -1,2 +1,6 @@
 # Log output
-Deploy with `kubectl apply -f manifests/deployment.yaml`
+Deploy with
+```
+kubectl apply -f manifests
+```
+To test the service open `http://localhost:8081` in your browser. You should get a timestamp with a random string.
--- log-output/index.js
+++ log-output/index.js
@@ -1,8 +1,28 @@
 const crypto = require('crypto')
+const express = require('express')
+require('dotenv').config()
 
+const PORT = process.env.PORT
 
-let uuid = crypto.randomUUID()
-const interval = setInterval(() => {
+const uuid = crypto.randomUUID()
+
+const status = () => {
   const now = new Date()
-  console.log(now.toISOString() + ': ' + uuid)
+  return now.toISOString() + ': ' + uuid
+}
+
+const interval = setInterval(() => {
+  console.log(status())
 }, 5000)
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', async (_req, res) => {
+  res.send(status())
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
