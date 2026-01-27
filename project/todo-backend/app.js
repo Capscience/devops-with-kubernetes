c00552e665b8136b8034e9aa79bd8258dd0141b5
Simo Soini <simo@capscience.fi>
2026-01-27 12:00:39 +0200

    Exercise 4.2 submission

--- project/todo-backend/app.js
+++ project/todo-backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const todosRouter = require('./controllers/todos')
+const db = require('./db')
 
 const app = express()
 app.use(express.json())
@@ -13,7 +14,16 @@ morgan.token('post_data', (req) => {
   }
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_data'))
-
 app.use('/api/todos', todosRouter)
+
+app.get('/healthz', async (_req, res) => {
+  try {
+    await db.authenticate()
+    return res.status(200).end()
+  } catch (err) {
+    console.error(err)
+    return res.status(503).end()
+  }
+})
 
 module.exports = app
